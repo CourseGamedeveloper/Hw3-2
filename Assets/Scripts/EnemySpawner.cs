e8b8a5829e87400f3785a6f1a4d6a1f7413a91bd
IbrahemHurani <86603326+IbrahemHurani@users.noreply.github.com>
2024-12-01 14:31:27 +0200

    Update EnemySpawner.cs

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -3,12 +3,11 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] private GameObject enemyPrefab;
-    [SerializeField]
-    [Tooltip("the minimum time for the enemy to appear")]
+    [SerializeField] [Tooltip("The minimum time for the enemy to appear")]
     private float minimumSpawnTime = 0f;
-    [SerializeField]
-    [Tooltip("the maximum time for the enemy to appear")]
-    private float maximumSpawnTime=2f;
+    [SerializeField] [Tooltip("The maximum time for the enemy to appear")]
+    private float maximumSpawnTime = 2f;
+
     private float timeUntilSpwan;
 
     void Start()
@@ -16,20 +15,19 @@ public class EnemySpawner : MonoBehaviour
         SetTimeSpwan();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        //spawner enemy
-        timeUntilSpwan-=Time.deltaTime;
-        if(timeUntilSpwan <=0)
+        // Spawner enemy
+        timeUntilSpwan -= Time.deltaTime;
+        if (timeUntilSpwan <= 0)
         {
-            Instantiate(enemyPrefab,transform.position,Quaternion.identity);
+            Instantiate(enemyPrefab, transform.position, Quaternion.identity);
             SetTimeSpwan();
-
         }
     }
+
     private void SetTimeSpwan()
     {
-        timeUntilSpwan=Random.Range(minimumSpawnTime, maximumSpawnTime);
+        timeUntilSpwan = Random.Range(minimumSpawnTime, maximumSpawnTime);
     }
 }
